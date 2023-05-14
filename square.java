20b86b47737636f2f5fb8e06c740a6e5fb5185d3
Amit <Amitd5877@gmail.com>
2023-05-14 19:55:18 +0530

    first commit

---
+++ square.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class square
+{
+    public static void main(String[] args) {
+
+        
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter no of rows:");
+        int n=sc.nextInt();
+
+        System.out.print("Enter no of Column:");
+        int m=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+
+
+
+
+    }
+}
